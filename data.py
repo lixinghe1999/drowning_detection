f89f629ea27698fe7ab45947742960fbb0edc492
Lixing He <helixing99@gmail.com>
2021-01-24 17:24:00 +0800

    1/24

--- data.py
+++ data.py
@@ -2,48 +2,36 @@
 analyze the response data
 '''
 import numpy as np
+from scipy.signal import find_peaks
 import matplotlib.pyplot as plt
 
-min_human=0.25
-max_human=4
+min_human=0.5
+max_human=2.5
 
-def filter(data, scan_range):
-    len_sample=scan_range/len(data)
-    len_filter=round(min_human/len_sample)
-    return np.convolve(data, np.ones((len_filter,)) / len_filter, mode="same")
-
-def detect(data, scan_range):
-    len_sample = scan_range / len(data)
-    len_block = round(max_human / len_sample)
-    var = np.empty(len(data))
-    mea = np.empty(len(data))
-    print(len_block)
-    for i in range(0,len(data),round(len_block)):
-        var[i:i+len_block]=np.var(data[i:i+len_block])
-        mea[i:i+len_block]=np.mean(data[i:i+len_block])
-    return np.logical_or(data < mea-2*var, data > mea+2*var)
-
-
-def adjust_detect(index):
-    a=0
-    peak=[]
-    for i in range(len(index)):
-        if index[i] and a==0:
-            a=1
-            peak.append(i)
-        elif a==1 and (not index[i]):
-            a=0
-            peak.append(i)
-    return peak
-
-
+def filter(data, len_sample, k):
+    if k == 0:
+        len_filter=round(min_human/len_sample)
+    else:
+        len_filter=round(max_human/len_sample)
+    return np.convolve(data, np.ones(len_filter)/len_filter, mode="same")
+def detect(data, len_sample, threshold):
+    max_peak = round(max_human / len_sample)
+    min_peak = round(min_human / len_sample)
+    peaks, dict = find_peaks(data,distance = min_peak, width = [min_peak,max_peak],prominence=3*threshold)
+    return peaks, dict
+def cal_size(widths, len_sample):
+    return [i*len_sample for i in widths]
 
 if __name__ == '__main__':
-    data=np.random.random((500))
-    data[30:50]+=8
-    data_filter=filter(data,25)
-    index=detect(data_filter,25)
-    peak=adjust_detect(index)
-    print(peak)
-    plt.plot(data[index])
-    #plt.show()
+    data=np.random.random((500))*0.5
+    data[200:500]+=2
+    data[30:50]+=1
+    data[350:372]+=1.5
+    scan_range = 25
+    len_sample = scan_range / len(data)
+    data_filter=filter(data, len_sample, 0)
+    local_var=filter(abs(data-data_filter), len_sample, 1)
+    peaks, dict=detect(data_filter,len_sample,local_var)
+    print(peaks, dict['widths'])
+    plt.plot(data_filter)
+    plt.show()
